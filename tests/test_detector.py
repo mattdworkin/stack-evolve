2324a59080de301948083201570cde9a47e9ce0e
Ignacio Galindo <ignaciogalindo111@gmail.com>
2026-03-05 20:52:03 -0600

    initial detector setup and detector tests

--- tests/test_detector.py
+++ tests/test_detector.py
@@ -1,3 +1,12 @@
 # File will be used later. To run tests, navigate to repo root and input: pytest
-def test_placeholder():
-    assert True
+
+
+from detector.flask_detector import is_flask_app
+
+def test_detects_flask_app():
+    assert is_flask_app("sample_apps/flask_simple/app.py") is True
+   
+
+def test_rejects_non_flask_app():
+    assert is_flask_app("sample_apps/non_flask_simple/app.py") is False
+    
